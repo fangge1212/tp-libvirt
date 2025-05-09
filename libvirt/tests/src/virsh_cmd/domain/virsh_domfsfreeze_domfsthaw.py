a5ce9d45c93377f45e57287c3707ecf16bbacc12
Fangge Jin <fjin@redhat.com>
2025-05-09 18:50:44 +0800

    guest_agent: Add more logic to domfsfreeze_domfsthaw
    Add more logic to domfsfreeze_domfsthaw, so it can cover more
    checkpoints and scenarios.
    
    Signed-off-by: Fangge Jin <fjin@redhat.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_domfsfreeze_domfsthaw.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_domfsfreeze_domfsthaw.py
@@ -17,12 +17,13 @@ def run(test, params, env):
     6) Retouch the file the ensure guest file system are not frozen;
     7) Cleanup test environment.
     """
-    def check_freeze(session):
+    def check_freeze(session, file_name):
         """
         Check whether file system has been frozen by touch a test file
         and see if command will hang.
 
         :param session: Guest session to be tested.
+        :param file_name: The name of file to be created on guest.
         """
         try:
             output = session.cmd_output('touch freeze_test',
@@ -32,24 +33,70 @@ def run(test, params, env):
         except aexpect.ShellTimeoutError:
             pass
 
-    def check_thaw(session):
+    def check_thaw(session, file_name):
         """
         Check whether file system has been thawed by check a test file
         prohibited from creation when frozen created and successfully touch
         the file again.
 
         :param session: Guest session to be tested.
+        :param file_name: The name of file to be checked on guest.
         """
-        status, output = session.cmd_status_output('ls freeze_test')
+        status, output = session.cmd_status_output('ls %s' % file_name)
         if status:
             test.fail("Failed to thaw file system. "
                       "Find created file failed:\n%s" % output)
 
         try:
-            output = session.cmd_output('touch freeze_test', timeout=10)
+            output = session.cmd_output('touch %s' % file_name, timeout=10)
         except aexpect.ShellTimeoutError:
-            test.fail("Failed to freeze file system. "
+            test.fail("Failed to thaw file system. "
                       "Touch file timeout:\n%s" % output)
+
+    def generate_random_string(length=10):
+        """
+        Generate a random string with specified length.
+        """
+        import random
+        import string
+
+        return ''.join(random.choice(string.ascii_letters + string.digits)
+                       for _ in range(length))
+
+    def run_agent_command_when_frozen(vm_name, command_name='domtime'):
+        """
+        Run qemu guest agent command when guest filesystem is fronzen.
+        It should fail with reasonable error.
+
+        Args:
+            vm_name (string): Vm name
+            command_name (string): Qemu agent command name, e.g. domtime
+        Returns:
+            None
+        """
+        fail_patts = [
+            r"error: guest agent command failed: unable to execute QEMU agent command '\S+': Command guest-get-time has been disabled: the command is not allowed",
+            r"error: internal error: unable to execute QEMU agent command '\S+': Command guest-get-time has been disabled: the command is not allowed"
+        ]
+
+        virsh_command = eval('virsh.%s' % command_name)
+        res = virsh_command(vm_name)
+        libvirt.check_result(res, fail_patts)
+
+    def run_agent_command_when_thawed(vm_name, command_name='domtime'):
+        """
+        Run qemu guest agent command when guest filesystem is thawed.
+        It should succeed.
+
+        Args:
+            vm_name (string): Vm name
+            command_name (string): Qemu agent command name, e.g. domtime
+        Returns:
+            None
+        """
+        virsh_command = eval('virsh.%s' % command_name)
+        res = virsh_command(vm_name)
+        libvirt.check_exit_status(res, expect_error=False)
 
     def cleanup(session):
         """
@@ -71,6 +118,7 @@ def run(test, params, env):
     mountpoint = params.get("mountpoint", None)
     vm_name = params.get("main_vm")
     vm = env.get_vm(vm_name)
+    file_name = generate_random_string() + "_freeze_test"
 
     xml_backup = vm_xml.VMXML.new_from_inactive_dumpxml(vm_name)
     try:
@@ -96,12 +144,16 @@ def run(test, params, env):
             res = virsh.domfsfreeze(vm_name, mountpoint=mountpoint)
             libvirt.check_result(res, fail_patts, skip_patts)
             if not res.exit_status:
-                check_freeze(session)
+                check_freeze(session, file_name)
 
-            res = virsh.domfsthaw(vm_name, mountpoint=mountpoint)
+            run_agent_command_when_frozen(vm_name)
+
+            res = virsh.domfsthaw(vm_name)
             libvirt.check_result(res, fail_patts, skip_patts)
             if not res.exit_status:
-                check_thaw(session)
+                check_thaw(session, file_name)
+
+            run_agent_command_when_thawed(vm_name)
 
             cleanup(session)
         finally:
